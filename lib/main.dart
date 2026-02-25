9871ccb1b79f5475bd857ebda877f751395c229a
awambua <cantosauric@gmail.com>
2026-02-25 11:02:06 +0300

    Week 5

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrangeAccent,
+          title: Text("Logging Page", style: TextStyle(color: Colors.white)),
+          centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          //crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              "My application",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 30,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+            Text("Enter Username"),
+            TextField(),
+            Text("Enter Password"),
+            TextField(),
+            SizedBox(height: 20),
+            MaterialButton(
+              onPressed: () {},
+              child: Text("Login", style: TextStyle(color: Colors.white)),
+              color: Colors.deepOrangeAccent,
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
