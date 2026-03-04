dc6ba3651edb1832cc04e96e277f4482f60efb9a
awambua <cantosauric@gmail.com>
2026-03-04 11:01:07 +0300

    Week6-1

--- lib/main.dart
+++ lib/main.dart
@@ -6,25 +6,25 @@ void main() {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.deepOrangeAccent,
-          title: Text("Logging Page", style: TextStyle(color: Colors.white)),
-          centerTitle: true,
-        ),
+        // appBar: AppBar(
+        //   backgroundColor: Colors.deepOrangeAccent,
+        //   title: Text("Logging Page", style: TextStyle(color: Colors.white)),
+        //   centerTitle: true,
+        // ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                "Jumia Marketplace",
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 30,
-                  fontWeight: FontWeight.w800,
-                ),
-              ),
-              Image.asset("assets/jumia_logo.png", height: 200, width: 200),
+              // Text(
+              //   "Jumia Marketplace",
+              //   style: TextStyle(
+              //     color: Colors.blue,
+              //     fontSize: 30,
+              //     fontWeight: FontWeight.w800,
+              //   ),
+              // ),
+              Image.asset("assets/jumia_logo.png", width: 200),
               Padding(
                 padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
                 child: Row(
@@ -52,7 +52,22 @@ void main() {
                   ),
                 ),
               ),
-              Text("Enter Password"),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Enter Password",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               Padding(
                 padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
                 child: TextField(
@@ -66,10 +81,27 @@ void main() {
                 ),
               ),
               SizedBox(height: 20),
-              MaterialButton(
-                onPressed: () {},
-                child: Text("Login", style: TextStyle(color: Colors.white)),
-                color: Colors.deepOrangeAccent,
+              // MaterialButton(
+              //   onPressed: () {},
+              //   child: Text("Login", style: TextStyle(color: Colors.white)),
+              //   color: Colors.deepOrangeAccent,
+
+              // ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white, fontSize: 14),
+                  ),
+                ),
               ),
             ],
           ),
