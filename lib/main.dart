c0bb2b313ee85fbeba23e2a95039497112ae3931
awambua <cantosauric@gmail.com>
2026-03-04 10:25:39 +0300

    Week6-1

--- lib/main.dart
+++ lib/main.dart
@@ -5,33 +5,74 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
           backgroundColor: Colors.deepOrangeAccent,
           title: Text("Logging Page", style: TextStyle(color: Colors.white)),
           centerTitle: true,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              "My application",
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 30,
-                fontWeight: FontWeight.w800,
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "Jumia Marketplace",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w800,
+                ),
               ),
-            ),
-            Text("Enter Username"),
-            TextField(),
-            Text("Enter Password"),
-            TextField(),
-            SizedBox(height: 20),
-            MaterialButton(
-              onPressed: () {},
-              child: Text("Login", style: TextStyle(color: Colors.white)),
-              color: Colors.deepOrangeAccent,
-            ),
-          ],
+              Image.asset("assets/jumia_logo.png", height: 200, width: 200),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Enter Username",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hint: Text("Email or Phone Number"),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                ),
+              ),
+              Text("Enter Password"),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hint: Text("PIN or Password"),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    prefixIcon: Icon(Icons.lock),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              MaterialButton(
+                onPressed: () {},
+                child: Text("Login", style: TextStyle(color: Colors.white)),
+                color: Colors.deepOrangeAccent,
+              ),
+            ],
+          ),
         ),
       ),
     ),
